f807ad4758187040ba7239751a05650fa8cab442
devs <144305887+hiepthhe161790@users.noreply.github.com>
2025-08-07 13:44:10 +0700

    done

--- src/main/java/com/example/jav_projecto1/config/CorsConfig.java
+++ src/main/java/com/example/jav_projecto1/config/CorsConfig.java
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+        registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
